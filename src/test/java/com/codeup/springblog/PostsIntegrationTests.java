636f7cb469abd93b730223df9627adfe6be50c8e
alyssalanae <briggs.l.alyssa@gmail.com>
2020-02-05 09:06:19 -0600

    Working

--- src/test/java/com/codeup/springblog/PostsIntegrationTests.java
+++ src/test/java/com/codeup/springblog/PostsIntegrationTests.java
@@ -1,6 +1,8 @@
 package com.codeup.springblog;
 
+import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
@@ -36,6 +38,9 @@ public class PostsIntegrationTests {
 	private UserRepository userDao;
 
 	@Autowired
+	private PostRepository postDao;
+
+	@Autowired
 	private PasswordEncoder pen;
 
 	private User testUser;
@@ -62,5 +67,28 @@ public class PostsIntegrationTests {
 				.andReturn()
 				.getRequest()
 				.getSession();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		setUpTestUser();
+		setUpSession();
+	}
+
+	//testing for a posts creation//
+	@Test
+	public void testPostCreation() throws Exception {
+		mvc.perform(
+				post("/posts/create").with(csrf())
+				.session((MockHttpSession) session)
+				.param("title", "Test Title")
+				.param("body", "This is a test post.")
+		)
+				.andExpect(status().is3xxRedirection())
+				.andDo(print());
+
+		//clean up
+		Post p = postDao.findByTitle("Test Title");
+		postDao.deleteById(p.getId());
 	}
 }
